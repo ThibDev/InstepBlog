3ef4c85f0a737d4de056699f84be1d2ac45f242a
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-24 10:15:52 +0200

    add testHomepage function

--- tests/src/Controller/HomepageControllerTest.php
+++ tests/src/Controller/HomepageControllerTest.php
@@ -3,6 +3,7 @@
 namespace App\tests\src\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\HttpFoundation\Response;
 
 class HomepageControllerTest extends WebTestCase{
 
@@ -10,8 +11,12 @@ class HomepageControllerTest extends WebTestCase{
      * Permet de vérifier que lorsque l'on tape / on obtient une réponse 200
      * @return void
      */
-    
-    public function TestHomepage(){
+
+    public function testHomepage(){
+
+        $client = static::createClient();
+        $crawler = $client->request('GET', '/');
+        self::assertResponseStatusCodeSame(Response::HTTP_OK);
 
     }
 }
