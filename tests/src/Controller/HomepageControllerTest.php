ed33b86d61e3244fe9fe3707d3eba87e6a000634
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-24 10:52:28 +0200

    add testMainTitleOnHomepage

--- tests/src/Controller/HomepageControllerTest.php
+++ tests/src/Controller/HomepageControllerTest.php
@@ -5,18 +5,37 @@ namespace App\tests\src\Controller;
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 use Symfony\Component\HttpFoundation\Response;
 
-class HomepageControllerTest extends WebTestCase{
+class HomepageControllerTest extends WebTestCase
+{
+    private function requestHomepage(){
 
-    /**
-     * Permet de vérifier que lorsque l'on tape / on obtient une réponse 200
-     * @return void
-     */
-
-    public function testHomepage(){
-
+        /**
+         * Permet d'effectuer une requete en get sur /
+         * @return void
+         */
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
-        self::assertResponseStatusCodeSame(Response::HTTP_OK);
+        return $client->request('GET', '/');
+    }
 
+    public function testHomepage()
+    {
+
+        /**
+         * Permet de vérifier que lorsque l'on tape / on obtient une réponse 200
+         * @return void
+         */
+        $this->requestHomepage();
+        self::assertResponseStatusCodeSame(Response::HTTP_OK);
+    }
+
+    public function testMainTitleOnHomepage()
+    {
+
+        /**
+         * Permet de tester la présence d'un h1 sur l'accueil
+         * @return void
+         */
+        $this->requestHomepage();
+        self::assertSelectorTextContains('h1', 'Bienvenue sur Instep-Blog');
     }
 }
