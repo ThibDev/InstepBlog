e79482f7ea7559e947fef85eeea5754a9f1b507b
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-24 15:45:13 +0200

    add hasher on user and admin fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,15 +5,27 @@ namespace App\DataFixtures;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    private UserPasswordHasherInterface $encoder;
+
+    private function __construct(UserPasswordHasherInterface $encoder)
+    {
+        $this->encoder = $encoder;
+    }
+
     public function load(ObjectManager $manager): void
     {
         $user = new User();
-        $user->setEmail('test@gmail.com')->setPassword('admin');
+        $user->setEmail('test@gmail.com')->setPassword($this->encoder->hashPassword($user, 'test'));
+        $manager->persist($user);
+
         $adminUser = new User();
-        $adminUser->setEmail('admin@gmail.com')->setPassword('admin');
+        $adminUser->setEmail('admin@gmail.com')->setRoles(["ROLE_ADMIN"])->setPassword($this->encoder->hashPassword($adminUser, 'admin'));
+        $manager->persist($adminUser);
 
         $manager->flush();
     }
