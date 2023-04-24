286a3e7edb23b78ad5ecabb22779fce7276bd0ee
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-24 10:11:11 +0200

    add HomepageControllerTest

---
+++ tests/src/Controller/HomepageControllerTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\tests\src\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class HomepageControllerTest extends WebTestCase{
+
+    /**
+     * Permet de vérifier que lorsque l'on tape / on obtient une réponse 200
+     * @return void
+     */
+    
+    public function TestHomepage(){
+
+    }
+}
