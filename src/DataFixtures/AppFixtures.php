3a9bb49eac8862e5893467dc42ec3df9136a5691
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-24 15:33:50 +0200

    add user and admin fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
@@ -9,8 +10,10 @@ class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = new User();
+        $user->setEmail('test@gmail.com')->setPassword('admin');
+        $adminUser = new User();
+        $adminUser->setEmail('admin@gmail.com')->setPassword('admin');
 
         $manager->flush();
     }
