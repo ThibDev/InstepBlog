8036c1229856a4d430246e4ce9c3d3f9fcc4bc0f
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-25 14:29:09 +0200

    add modifs on AdminAccountController

--- src/Controller/Admin/AdminAccountController.php
+++ src/Controller/Admin/AdminAccountController.php
@@ -5,14 +5,33 @@ namespace App\Controller\Admin;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AdminAccountController extends AbstractController
 {
     #[Route('/admin/connexion', name: 'admin_account_login')]
-    public function index(): Response
+    public function login(AuthenticationUtils $authenticationUtils): Response
     {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
         return $this->render('Admin/account/index.html.twig', [
-            'current_name' => 'homepage',
+        'last_username' => $lastUsername,
+        'error' => $error,
+        'current_menu' => 'homepage'
         ]);
+
+    }
+
+    #[Route(path: '/admin/logout', name: 'admin_account_logout')]
+    public function logout(): void
+    {
+        throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
     }
 }
