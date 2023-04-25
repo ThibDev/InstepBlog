4f689ed0258c9aac4af2623d443e51d13b06c468
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-25 11:11:18 +0200

    add register security

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -2,15 +2,37 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegisterType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class RegisterController extends AbstractController
 {
     #[Route('/inscription', name: 'register')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $encoder): Response
     {
+        $notification = null;
+        $user = new User();
+        $form = $this->createForm(RegisterType::class, $user);
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()){
+            $user = $form->getData();
+            $search_email = $entityManager->getRepository(User::class)->findOneByEmail($user->getEmail());
+            if(!$search_email){
+                $password = $encoder->hashPassword($user, $user->getPassword());
+                $user->setPassword($password);
+                $entityManager->persist($user);
+                $entityManager->flush();
+            }else{
+                $notification = "l'email saisi est déjà utilisé sur le site";
+            }
+        }
+
         return $this->render('register/index.html.twig', [
             'current_menu' => 'homepage',
         ]);
