bea00cc5d3bdd009e0e664198db2ef10f6623add
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-25 10:13:38 +0200

    add modifs to RegisterType

--- src/Form/RegisterType.php
+++ src/Form/RegisterType.php
@@ -4,6 +4,10 @@ namespace App\Form;
 
 use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,9 +16,43 @@ class RegisterType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
+            ->add('email', EmailType::class, [
+                'label' => 'Votre email',
+                'attr' => [
+                    'placeholder' => 'Merci de saisir vôtre email',
+                    'class' => 'form-control my-2'
+                ],
+                'label_attr' => [
+                    'class' => 'text-info'
+                ],
+                'required' => true,
+                'invalide_message' => "l\'email est obligatoire"
+            ])
+            ->add('password', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'required' => true,
+                'invalide_message' => 'Le mot de passe et la confirmation doivent être identiques',
+                'first_options' => [
+                    'label' => 'Votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de saisir votre mot de passe',
+                        'class' => 'form-control my-2'
+                    ]
+                ],
+                'second_options' => [
+                    'label' => 'Confirmer votre mot de passe',
+                    'attr' => [
+                        'placeholder' => 'Merci de confirmer votre mot de passe',
+                        'class' => 'form-control my-2'
+                    ]
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 's\'inscrire',
+                'attr' => [
+                    'class' => 'btn btn-lg btn-outline-primary mt-4'
+                ]
+            ])
         ;
     }
 
