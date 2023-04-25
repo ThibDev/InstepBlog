bca917e663f81982930fed8fe82f077523903d27
Thib Dev <thibaultdevinstep@gmail.com>
2023-04-25 16:49:41 +0200

    add ThemesType

--- src/Form/ThemesType.php
+++ src/Form/ThemesType.php
@@ -4,6 +4,9 @@ namespace App\Form;
 
 use App\Entity\Themes;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,10 +15,42 @@ class ThemesType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('title')
-            ->add('description')
-            ->add('illustration')
-            ->add('slug')
+            ->add('title', TextType::class, [
+                'label' => 'Nom du thème',
+                'attr' => [
+                    'placeholder' => 'Veuillez saisir le nom du thème',
+                    'class' => 'form-control my-2'
+                ],
+                'label_attr' => [
+                    'class' => 'text-info'
+                ]
+            ])
+            ->add('description', TextareaType::class, [
+                'label' => 'Description du thème',
+                'attr' => [
+                    'placeholder' => 'Veuillez saisir la description du thème',
+                    'class' => 'form-control my-2'
+                ],
+                'label_attr' => [
+                    'class' => 'text-info'
+                ]
+            ] )
+            ->add('illustration', TextType::class, [
+                'label' => 'Image du thème',
+                'attr' => [
+                    'placeholder' => 'Veuillez saisir l\'url de l\'image du thème',
+                    'class' => 'form-control my-2'
+                ],
+                'label_attr' => [
+                    'class' => 'text-info'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => 'Enregistrer',
+                'attr' => [
+                    'class' => 'btn btn-lg btn-outline-primary mt-4'
+                ]
+            ])
         ;
     }
 
